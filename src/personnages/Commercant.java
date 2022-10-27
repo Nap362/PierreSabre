fa0f3f76876c5478fc172a4e5744bf0b9234c5fd
Nap362 <ninonapv@gmail.com>
2022-10-27 14:49:03 +0200

    creation de la classe Yakuza et Ronin

--- src/personnages/Commercant.java
+++ src/personnages/Commercant.java
@@ -1,22 +1,21 @@
 package personnages;
 
-public class Commercant extends Humain{
+public class Commercant extends Humain {
 
 	public Commercant(String nom, int argent) {
-		super(nom,"thé", argent);
+		super(nom, "thé", argent);
 	}
-	
+
 	public int seFaireExtorquer() {
 		int vol = argent;
 		parler("J’ai tout perdu! Le monde est trop injuste...");
 		perdreArgent(argent);
 		return vol;
 	}
-	
+
 	public void recevoir(int argent) {
 		gagnerArgent(argent);
-		String texte = argent + " sous ! Je te remercie généreux donateur!";
-		parler(texte);
+		parler(argent + " sous ! Je te remercie généreux donateur!");
 	}
 
 }
